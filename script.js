e8487cf9eab57f681cb75a47b8341cd9fb55526f
benlepsch <blepsch@gmail.com>
2019-10-30 12:01:50 -0400

    test box style

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-let test = 5;
+/*
+    function onclick -> defined for each div that makes up the grid
+
+    onclick() {
+        if (flagged) return 0
+        if (is bomb) return 'you lose'
+        if (is not bomb) return getNeighbors(box.x, box.y)
+    }
+*/
+
+function generateGrid() {
+
+}
+
+function makeBox() {
+    let box = document.createElement('div');
+}
