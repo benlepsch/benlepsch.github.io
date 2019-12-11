2c1d6ccf46446023ea99fbcf918b5a29dce36583
benlepsch <blepsch@gmail.com>
2019-12-11 11:58:39 -0500

    try high score storing in cookie

--- beanjump/script.js
+++ beanjump/script.js
@@ -614,9 +614,11 @@ window.onload = function() {
 
 	for (let i = 0; i < c.length; i++) {
 		if (c[i].slice(0,10) == 'highscore=') {
-			best_score = parseInt(c[i].slice(10, c.length));
+			best_score = (c[i].slice(10, c.length));
+			console.log('setting best score to ' + c[i].slice(10, c.length));
 		} else if (c[i].slice(0, 11) == ' highscore=') {
-			best_score = parseInt(c[i].slice(11, c.length));
+			best_score = (c[i].slice(11, c.length));
+			console.log('setting best score to ' + c[i].slice(11, c.length));
 		}
 	}
 }
