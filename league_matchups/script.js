7db0ee72c00f55e3a3c6aca6d8c9bc24c75c144a
Ben Lepsch <blepsch@gmail.com>
2021-12-02 13:07:52 -0500

    try this

--- league_matchups/script.js
+++ league_matchups/script.js
@@ -1,3 +1,16 @@
-function clicked() {
-    alert('clicked');
+let champ = 'chogath';
+
+function chogath() {
+    champ = 'chogath';
+    document.getElementById('currentChamp').innerHTML = 'viewing matchups for: chogath';
+}
+
+function riven() {
+    champ = 'riven';
+    document.getElementById('currentChamp').innerHTML = 'viewing matchups for: riven';
+}
+
+function sion() {
+    champ = 'sion';
+    document.getElementById('currentChamp').innerHTML = 'viewing matchups for: sion';
 }
